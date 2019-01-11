5c93e9522d66f63db6ee052f112cd420fe295909
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-11 18:29:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
-var 'hello' = hello
+var 'hello' = "I can't hear you!"
 
 function sayHiToGrandma(string) {
   if (string === 'hello') {
