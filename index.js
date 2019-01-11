d1d266f070910ab270d03c5ae303f90a2288a8f7
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-11 17:03:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  console.log("I can't hear you!")
-  return string.toLowerCase
+  console.log('hello')
+  return "I can\'t hear you!"
 }
