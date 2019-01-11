2c8197f6cc00787eef31ab8973a4706532a615d0
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-11 18:38:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string) {
   if (string === "I love you, Grandma."){ 
     return "I love you, too.";
   }
-  else if (string === string.toUpperCase(_)){
+  else if (string === string.toUpperCase()){
     return "YES INDEED!";
   }
   else {
