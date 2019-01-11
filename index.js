51edd2ab81b8924d72a4774ecaad9d05a245fbc7
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-11 18:34:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  if (string = "I love you, Grandma.")  { return 
+  if (string === "I love you, Grandma.")  { 
+    return "I love you, too."
+  }
+  else if (string === string.toUpperCase) {
+    return "YES INDEED!"
+  }
+  else (string === string.toLowerCase) {
+    return "I can't hear you!"
   }
 }
   
